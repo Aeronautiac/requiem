4d93ef3d2e9420cb6b02e4e4487ebb8939c889b2
Aeronautiac <ibanezmatteo1@gmail.com>
2026-06-23 02:38:54 -0400

    more ui experimentation - theres currently a bug with how adding players is handled. players created later do not currently see the full player list. this kind of thing might become an issue for things like game announcements and such later as well if a player is added mid game on the engine level. need to find a way to deal with this kind of thing.

--- armonia/src/app/mod.rs
+++ armonia/src/app/mod.rs
@@ -6,7 +6,7 @@ use std::{
 use eframe::App;
 use egui::{Align, ComboBox, Layout, Panel, Popup, PopupCloseBehavior, Ui, ViewportCommand};
 use lawliet_types::{
-    action::{Action, ActionActor, ActionRequest, AddPlayer},
+    action::{Action, ActionActor, ActionRequest, ActionResponse, AddPlayer},
     common::ActorKey,
     role::Role,
 };
@@ -30,19 +30,12 @@ struct Player {
     privileged_info: Option<PrivilegedPlayerInfo>,
 }
 
+#[derive(Default)]
 struct GameViewport {
     players: HashMap<ActorKey, Player>,
 }
 
-impl Default for GameViewport {
-    fn default() -> Self {
-        Self {
-            players: HashMap::new(),
-        }
-    }
-}
-
-#[derive(Hash)]
+#[derive(Hash, PartialEq, Eq, Debug, Clone, Copy)]
 enum Viewer {
     Admin,
     Player(ActorKey),
@@ -52,21 +45,52 @@ struct GameState {
     views: HashMap<Viewer, GameViewport>,
 }
 
-impl Default for GameState {
-    fn default() -> Self {
-        Self {
-            views: HashMap::new(),
+impl GameState {
+    // add view for the player, add info to everyone else's views, only show privileged info to the
+    // player and the admin
+    fn add_player(&mut self, key: ActorKey, settings: &NewPlayerSettings) {
+        // create view
+        self.views
+            .insert(Viewer::Player(key), GameViewport::default());
+
+        // update views
+        for (viewer, view) in self.views.iter_mut() {
+            let mut privileged = None;
+            if *viewer == Viewer::Admin || *viewer == Viewer::Player(key) {
+                privileged = Some(PrivilegedPlayerInfo {
+                    role: settings.role,
+                    true_name: settings.true_name.clone(),
+                });
+            }
+
+            let player = Player {
+                id: PlayerIdentity {
+                    display_name: settings.display_name.clone(),
+                    key,
+                },
+                privileged_info: privileged,
+            };
+
+            view.players.insert(key, player);
         }
+    }
+
+    fn admin_view(&self) -> &GameViewport {
+        &self.views[&Viewer::Admin]
     }
 }
 
-#[derive(Clone, Copy)]
-enum UserInput {
-    Exit,
-    AddPlayer,
+impl Default for GameState {
+    fn default() -> Self {
+        let mut views = HashMap::new();
+        views.insert(Viewer::Admin, GameViewport::default());
+        Self { views }
+    }
 }
 
+#[derive(Clone)]
 struct NewPlayerSettings {
+    display_name: String,
     true_name: String,
     role: Role,
 }
@@ -74,16 +98,23 @@ struct NewPlayerSettings {
 impl Default for NewPlayerSettings {
     fn default() -> Self {
         Self {
+            display_name: String::new(),
             true_name: String::new(),
             role: Role::Civilian,
         }
     }
 }
 
+#[derive(Clone)]
+enum UserInput {
+    Exit,
+    AddPlayer(NewPlayerSettings),
+}
+
 struct UIState {
     input: Option<UserInput>,
     waiting_input: Option<UserInput>,
-    selected_actor: ActionActor,
+    selected_viewer: Viewer,
     new_player_state: NewPlayerSettings,
     show_add_players_popup: bool,
 }
@@ -93,7 +124,7 @@ impl Default for UIState {
         Self {
             input: None,
             waiting_input: None,
-            selected_actor: ActionActor::Admin,
+            selected_viewer: Viewer::Admin,
             new_player_state: NewPlayerSettings::default(),
             show_add_players_popup: false,
         }
@@ -131,15 +162,15 @@ impl Application {
             .as_millis();
 
         // dont process inputs until the current one has been responded to
-        if let Some(input) = self.ui_state.input
+        if let Some(input) = self.ui_state.input.clone()
             && self.ui_state.waiting_input.is_none()
         {
-            match input {
+            match &input {
                 UserInput::Exit => {
                     self.exit();
                     ctx.send_viewport_cmd(ViewportCommand::Close);
                 }
-                UserInput::AddPlayer => self
+                UserInput::AddPlayer(_) => self
                     .input_wrt
                     .send(ActionRequest {
                         timestamp: time,
@@ -161,8 +192,20 @@ impl Application {
             match execution.exec_result {
                 AppExecResult::Standard(result) => {
                     if let Ok(response) = result {
-                        if matches!(execution.action_req.payload, Action::AddPlayer(_)) {
-                            dbg!("received response to add player");
+                        // if we get a response with no waiting input, something is wrong
+                        match self
+                            .ui_state
+                            .waiting_input
+                            .as_mut()
+                            .expect("received a response with no waiting input")
+                        {
+                            UserInput::AddPlayer(settings) => {
+                                let ActionResponse::AddPlayer(response_data) = response.0 else {
+                                    unreachable!()
+                                };
+                                self.game_state.add_player(response_data.id, settings);
+                            }
+                            _ => {}
                         }
                     } else {
                         dbg!("error");
@@ -180,13 +223,17 @@ impl Application {
     // UI HELPERS
     fn player_selection(&mut self, ui: &mut Ui) {
         ComboBox::from_label("Player Selection")
-            .selected_text(format!("{:?}", self.ui_state.selected_actor))
+            .selected_text(format!("{:?}", self.ui_state.selected_viewer))
             .show_ui(ui, |ui| {
-                ui.selectable_value(
-                    &mut self.ui_state.selected_actor,
-                    ActionActor::Admin,
-                    "Admin",
-                );
+                ui.selectable_value(&mut self.ui_state.selected_viewer, Viewer::Admin, "Admin");
+
+                for (key, player) in self.game_state.admin_view().players.iter() {
+                    ui.selectable_value(
+                        &mut self.ui_state.selected_viewer,
+                        Viewer::Player(*key),
+                        &player.id.display_name,
+                    );
+                }
             });
 
         let button_response = ui.button("Add Players");
@@ -197,6 +244,11 @@ impl Application {
             .open_bool(&mut self.ui_state.show_add_players_popup)
             .close_behavior(PopupCloseBehavior::IgnoreClicks) // <- no more auto-close on any click
             .show(|ui| {
+                ui.horizontal(|ui| {
+                    ui.label("Display Name");
+                    ui.text_edit_singleline(&mut self.ui_state.new_player_state.display_name);
+                });
+
                 ui.horizontal(|ui| {
                     ui.label("True Name");
                     ui.text_edit_singleline(&mut self.ui_state.new_player_state.true_name);
@@ -216,13 +268,29 @@ impl Application {
 
                 ui.horizontal(|ui| {
                     if ui.button("Add").clicked() {
-                        self.ui_state.input = Some(UserInput::AddPlayer);
+                        self.ui_state.input =
+                            Some(UserInput::AddPlayer(self.ui_state.new_player_state.clone()));
                     }
                     if ui.button("Close").clicked() {
                         ui.close();
                     }
                 });
             });
+    }
+
+    fn player(&self, ui: &mut Ui, player: &Player) {
+        if ui.button(&player.id.display_name).clicked() {
+            dbg!("player interaction");
+        }
+    }
+
+    fn player_list(&mut self, ui: &mut Ui) {
+        for (id, player) in self.game_state.views[&self.ui_state.selected_viewer]
+            .players
+            .iter()
+        {
+            self.player(ui, player);
+        }
     }
 }
 
@@ -233,6 +301,15 @@ impl App for Application {
 
     fn ui(&mut self, ui: &mut egui::Ui, frame: &mut eframe::Frame) {
         self.ui_state.input = None;
+
+        Panel::top("top bar")
+            .resizable(false)
+            .show_inside(ui, |ui| {
+                ui.with_layout(
+                    Layout::centered_and_justified(egui::Direction::LeftToRight),
+                    |ui| ui.label("Requiem"),
+                );
+            });
 
         Panel::bottom("bottom bar")
             .resizable(false)
@@ -247,5 +324,15 @@ impl App for Application {
                     });
                 });
             });
+
+        Panel::right("right panel")
+            .resizable(true)
+            .show_inside(ui, |ui| {
+                self.player_list(ui);
+            });
+
+        Panel::left("left panel")
+            .resizable(true)
+            .show_inside(ui, |ui| {});
     }
 }
